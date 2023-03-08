ce1759be0eb4cab897bd3fa6afe31995d9d9537b
BrunoArrm <bruno.arrm@gmail.com>
2023-03-08 13:24:16 -0300

    Componente Footer foi estilizado

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,17 @@ function Footer() {
   return (
     <footer className='footer'>
       <Pagination />
+      <div className='socialMedias'>
+            <a href="https://github.com/BrunoArrm/pokedex" target="_blank">
+                <div className='socialMedia github'></div>
+            </a>
+            <a href="https://linkedin.com/in/bruno-arruda-dev" target="_blank">
+                <div className='socialMedia linkedin'></div>
+            </a>
+        </div>
+        <div className='sign'>
+            <p>Copyright © 2023 | <span>Bruno Arruda</span> | POKÉAPI | Todos os direitos reservados.</p>
+        </div>
     </footer>
   );
 }
