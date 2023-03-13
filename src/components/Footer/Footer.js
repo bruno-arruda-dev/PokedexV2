54376c1a5358cd3f7ae5abe7054ef98ffcfdcad0
BrunoArrm <bruno.arrm@gmail.com>
2023-03-12 21:32:54 -0300

    Corrigido links para as redes sociais e para o repositório no GitHub não abriam nova guia. Adicionado link para POKEAPI nos créditos

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,15 +7,15 @@ function Footer() {
     <footer className='footer'>
       <Pagination />
       <div className='socialMedias'>
-            <a href="https://github.com/BrunoArrm/pokedex">
+            <a href="https://github.com/BrunoArrm/PokedexV2" target="_blank">
                 <div className='socialMedia github'></div>
             </a>
-            <a href="https://linkedin.com/in/bruno-arruda-dev">
+            <a href="https://linkedin.com/in/bruno-arruda-dev" target="_blank">
                 <div className='socialMedia linkedin'></div>
             </a>
         </div>
         <div className='sign'>
-            <p>Copyright © 2023 | <span>Bruno Arruda</span> | POKÉAPI | Todos os direitos reservados.</p>
+            <p>Copyright © 2023 | <span>Bruno Arruda</span> | <a className='pokeapi' href="https://pokeapi.co/" target="_blank">POKÉAPI</a> | Todos os direitos reservados.</p>
         </div>
     </footer>
   );
