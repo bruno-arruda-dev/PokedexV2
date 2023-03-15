f740cb6d3c9ba0471e6369357dbf87a3bb387a37
BrunoArrm <bruno.arrm@gmail.com>
2023-03-15 15:12:54 -0300

    BattleStats foi reformulado para ajudar o entendimento do usuário

--- src/components/MyPokemon/BattleStats/GraphicLine/GraphicLine.js
+++ src/components/MyPokemon/BattleStats/GraphicLine/GraphicLine.js
@@ -13,14 +13,9 @@ function GraphicLine(props) {
 
   return (
     <div className='graphicLine'>
-        <div>
             <div className='content'><p>{content}</p></div>
-        </div>
         <div className='lineStat-container'>
             <div className={`lineStat ${content}`}></div>
-        </div>
-        <div>
-            <div className='value'><p>{value}</p></div>
         </div>
     </div>
   )
