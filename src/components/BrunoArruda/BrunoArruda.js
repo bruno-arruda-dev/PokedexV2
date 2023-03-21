f6004918fcd2f0c4ff9e203b7e83701241bdbfc3
Bruno Arruda <86083291+BrunoArrm@users.noreply.github.com>
2023-03-20 23:06:19 -0300

    Foi incluído no header a apresentação e agradecimentos

--- src/components/BrunoArruda/BrunoArruda.js
+++ src/components/BrunoArruda/BrunoArruda.js
@@ -3,14 +3,45 @@ import './BrunoArruda.css';
 
 function BrunoArruda() {
     const [isOpen, setIsOpen] = useState("closed");
+    const [stWord, setStWord] = useState("about");
+    const [ndWord, setNdWord] = useState("me");
 
     const handleOpenAbout = () => {
-        isOpen === "closed" ? setIsOpen("open") : setIsOpen("closed");
+        if (isOpen === "closed") {
+            setIsOpen("open");
+            setStWord("tank");
+            setNdWord("you");
+        } else {
+            setIsOpen("closed");
+            setStWord("about");
+            setNdWord("me");
+        }
     }
 
   return (
-    <div className={`brunoArruda ${isOpen}`} onClick={handleOpenAbout}>
-      <p>tank<span className='dot'>.</span><span className='me'>you</span></p>
+    <div className={`brunoArruda ${isOpen}`}>
+        <div className='brunoArruda--button' onClick={handleOpenAbout}>
+            <p>{stWord}<span className='dot'>.</span><span className='me'>{ndWord}</span></p>
+        </div>
+        <div className={`brunoArruda__content--${isOpen}`}>
+        <div className='socialMedias'>
+            <a href="https://github.com/BrunoArrm/PokedexV2" target="_blank">
+                <div className='socialMedia github'></div>
+            </a>
+            <a href="https://linkedin.com/in/bruno-arruda-dev" target="_blank">
+                <div className='socialMedia linkedin'></div>
+            </a>
+        </div>
+        <div className='sign'>
+        </div>
+        <p>Desenvolvido por Bruno Arruda</p>
+        <div className='motivacional__container'>
+            <p className='motivacional'>“A única maneira de fazer algo excelente é amar o que você faz. Se você ainda não a encontrou, continue procurando. Não se acomode” - Steve Jobs.</p>
+        </div>
+        <div className='sign'>
+            <p>Copyright © 2023 | <a className='pokeapi' href="https://pokeapi.co/" target="_blank">POKÉAPI</a> | Todos os direitos reservados.</p>
+        </div>
+        </div>
     </div>
   );
 }
